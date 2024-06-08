8e0136c8451945d4414b77c1bbbb1344f4b27d3f
harristheharris <anthonyharris7777@gmail.com>
2024-06-07 21:25:54 -0400

    first

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/Layout.jsx";
+import Home from "./pages/Home";
+import Blogs from "./pages/Blogs";
+import Contact from "./pages/Contact";
+import NoPage from "./pages/NoPage";
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
